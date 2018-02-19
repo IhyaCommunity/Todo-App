e4d2ee3a7bdc9d7529a982ff49a75a8f8cb5f440
Hannan Akbar <hannanakbar@hotmail.com>
2018-02-19 18:44:26 +0500

    Create dev env

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const less = require('gulp-less');
+const cleanCss = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
+const LessAutoprefix = require('less-plugin-autoprefix');
+const BrowserSync = require('browser-sync').create();
+
+var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+
+var lessDir = 'src/assets/less/';
+
+// Tasks: Minify and Compile
+
+gulp.task('html-minify', function () {
+    gulp.src('src/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('image-minify', () =>
+    gulp.src('src/assets/image/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/assets/image'))
+);
+
+gulp.task('less', function () {
+    gulp.src(lessDir + 'main.less')
+        .pipe(sourcemaps.init())
+        .pipe(less({
+            plugins: [autoprefix, require('less-plugin-glob')]
+        }))
+        .pipe(cleanCss())
+        .pipe(sourcemaps.write('/maps'))
+        .pipe(gulp.dest('dist/assets/css/'))
+        .pipe(BrowserSync.stream());
+});
+
+// Tasks: Watch, Build and Serve
+
+gulp.task('serve', function () {
+    BrowserSync.init({
+        server: "dist"
+    });
+    gulp.watch('src/*.html', ['html-minify']);
+    gulp.watch('src/assets/image/*', ['image-minify']);
+    gulp.watch(lessDir + '**/*.less', ['less']);
+    gulp.watch('dist/*.html').on('change', BrowserSync.reload);
+});
+
+
+// Tasks: Default Build Trigger
+
+gulp.task('default', ['html-minify', 'image-minify', 'less', 'serve']);
