d16a079ac1e3500b74c7cfebf96ed06ceaaaf114
Arslan Ameer <arslan.ameer@hotmail.com>
2018-03-10 02:24:00 +0500

    Fix Issue #7 Prevent Adding Duplicate Task
    Don't add duplicate items #7

--- src/assets/js/list.js
+++ src/assets/js/list.js
@@ -4,46 +4,84 @@
 
     var listInput = document.getElementById("add-input");
 
-    listInput.onfocus = function(){
+    listInput.onfocus = function () {
         this.removeAttribute("placeholder");
     }
-    
-    listInput.onblur = function(){
+
+    listInput.onblur = function () {
         this.placeholder = "Write Your Task Here!";
     }
 
     document.getElementById('add-button').addEventListener('click', (e) => {
-    
-        addItem();
-        listInput.value = null;        
-    
+        AddTask();
         e.preventDefault();
     });
-    
+
     listInput.addEventListener('keyup', (e) => {
-        
+
         if (e.key == 'Enter') {
-            addItem();
-            listInput.value = null;
-            listInput.placeholder = "Write Your Task Here!";
+            AddTask();
+            
         }
-    
+
         return false;
-    
+
         e.preventDefault();
     });
-    
+
     function addItem() {
+
         var li = document.createElement('li');
-    
+
         li.innerText = listInput.value;
-    
+        li.setAttribute("class", "listItem");
+
         li.addEventListener('click', (e) => {
             li.parentNode.removeChild(li);
         });
-    
+
         var ul = document.querySelector('.list ul');
         ul.insertBefore(li, ul.firstChild);
+    }
+
+    function checkDuplicateTask(inputValue, list) {
+        var status = 'false';
+
+        for (var i = 0; i < list.length; i++) {
+            var name = list[i].innerText;
+            if (name == inputValue) {
+                status = 'true';
+                break;
+            }
+        }
+
+        return status;
+    }
+
+    function AddTask() {
+
+        var listItems = document.getElementsByClassName("listItem");
+
+        if (listItems.length < 1) {
+            addItem();
+            listInput.value = null;
+        }
+        else {
+            var status = checkDuplicateTask(listInput.value, listItems);
+
+            switch (status) {
+                case 'true':
+                    alert("TASK Already Exist!");
+                    listInput.value = null;
+                    listInput.placeholder = "Write Your Task Here!";
+                    break;
+
+                case 'false':
+                    addItem();
+                    listInput.value = null;
+                    listInput.placeholder = "Write Your Task Here!";
+            }
+        }
     }
 
 })();
